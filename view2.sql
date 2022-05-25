af75d2c2546817e1518394fa6434b42e275df233
Tim <smithgate7@gmail.com>
2022-05-25 19:11:53 +0300

    changed view_2

--- view2.sql
+++ view2.sql
@@ -1,9 +1,11 @@
-/* View of projects per institution(Dummy) */
+/* View of projects per institution per year (used in Query_4) */
 
-CREATE VIEW projects_per_institution
-(ins_id, name, total_projects)
+CREATE VIEW projects_per_institution_per_year
+(ins_id, name, projects, year)
  AS
- SELECT i.ins_id, i.name, count(*) FROM institution i
+ SELECT i.ins_id, i.name, count(*), YEAR(a.date) as year FROM institution i
  INNER JOIN project p
  ON i.ins_id = p.ins_id
- GROUP BY i.ins_id
+ INNER JOIN assessment a
+ ON p.ass_id = a.ass_id
+ GROUP BY i.ins_id, year
