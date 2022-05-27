76a283c3973e03b3488ff5df7e296e9dca29174f
Tim <smithgate7@gmail.com>
2022-05-27 14:22:54 +0300

    in view2 project considered to be binded with institution on project start date

--- Query_4.sql
+++ Query_4.sql
@@ -4,4 +4,4 @@
 
 SELECT i.ins_id, i.name, i.year AS firts_year, dupl.year AS second_year, i.projects AS projects_each_year 
 FROM projects_per_institution_per_year i, projects_per_institution_per_year dupl
-WHERE i.ins_id = dupl.ins_id AND i.year = dupl.year - 1 AND i.projects = dupl.projects ANd i.projects >= 10  
+WHERE i.ins_id = dupl.ins_id AND i.year = dupl.year - 1 AND i.projects = dupl.projects AND i.projects >= 10  
--- view2.sql
+++ view2.sql
@@ -3,9 +3,7 @@
 CREATE VIEW projects_per_institution_per_year
 (ins_id, name, projects, year)
  AS
- SELECT i.ins_id, i.name, count(*), YEAR(a.date) as year FROM institution i
+ SELECT i.ins_id, i.name, count(*), YEAR(p.start) as year FROM institution i
  INNER JOIN project p
  ON i.ins_id = p.ins_id
- INNER JOIN assessment a
- ON p.ass_id = a.ass_id
- GROUP BY i.ins_id, year
+ GROUP BY i.ins_id, year;
