e3ce0d80934d1f2104c2d562007b12c35aad9625
Tim <smithgate7@gmail.com>
2022-05-29 18:32:51 +0300

    view_1 changed

--- view1.sql
+++ view1.sql
@@ -10,6 +10,9 @@ CREATE VIEW projects_per_researcher
  SELECT r.res_id, r.first_name, r.last_name, count(*) FROM researcher r
  INNER JOIN works w
  ON r.res_id = w.res_id
- INNER JOIN project p 
- ON w.proj_id = p.proj_id
  GROUP BY r.res_id
+ UNION
+ SELECT r.res_id, r.first_name, r.last_name, 0 FROM researcher r
+ LEFT OUTER JOIN works w
+ ON r.res_id = w.res_id
+ WHERE w.proj_id IS NULL;
