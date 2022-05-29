766c95e2e54442d05e3c2f5c654d3e1893f288bb
Tim <smithgate7@gmail.com>
2022-05-29 16:30:36 +0300

    Query_5 modified

--- Query_5.sql
+++ Query_5.sql
@@ -2,16 +2,14 @@
 		QUERY 5
 ======================*/
 
-SELECT DISTINCT project_field.field_name AS first_field_name, dupl.field_name AS second_field_name, count(*) AS Popularity FROM 
-(SELECT rf.field_name, pf.proj_id FROM research_field rf
- INNER JOIN proj_field pf
- ON rf.field_id = pf.field_id)  project_field
-INNER JOIN
-(SELECT rf.field_name, pf.proj_id FROM research_field rf  /* Reused.... */
- INNER JOIN proj_field pf
- ON rf.field_id = pf.field_id) dupl
-ON project_field.proj_id = dupl.proj_id
-WHERE project_field.field_name < dupl.field_name  /* Skipping Duplicates */
-GROUP BY project_field.field_name, dupl.field_name
-ORDER BY Popularity DESC
+WITH pf (field_name, field_id, proj_id) AS
+  (SELECT rf.field_name, rf.field_id, pf.proj_id FROM research_field rf
+   INNER JOIN proj_field pf 
+   ON rf.field_id = pf.field_id)
+SELECT pf1.field_name AS first_field, pf2.field_name AS second_field, count(*) AS projects_per_field_tuple FROM pf pf1 
+INNER JOIN pf pf2 
+ON pf1.proj_id = pf2.proj_id
+WHERE pf1.field_id < pf2.field_id  /* Skipping Duplicates */
+GROUP BY pf1.field_name, pf2.field_name
+ORDER BY projects_per_field_tuple DESC
 LIMIT 3;
