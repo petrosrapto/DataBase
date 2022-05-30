2bab3f239df8987ad7d33822739dc31dbafbf53f
Tim <smithgate7@gmail.com>
2022-05-30 16:05:04 +0300

    Query_3 changed(evaluator not included)

--- Query_3.sql
+++ Query_3.sql
@@ -9,22 +9,13 @@ INNER JOIN project p
 ON pf.proj_id = p.proj_id 
 WHERE rf.field_name = 'Engineering' AND p.start <= current_date()  AND p.end >= current_date() 
 
-(SELECT rf.field_name, r.res_id, r.first_name, r.last_name FROM research_field rf 
- INNER JOIN proj_field pf
- ON rf.field_id = pf.field_id
- INNER JOIN project p
- ON pf.proj_id = p.proj_id 
- INNER JOIN works w 
- ON p.proj_id = w.proj_id 
- INNER JOIN researcher r 
- ON w.res_id = r.res_id 
- WHERE rf.field_name = 'Engineering' AND (p.start <= current_date() AND p.end >= DATE_SUB(current_date(), INTERVAL 1 YEAR)) ) 
-UNION 
-(SELECT rf.field_name, r.res_id, r.first_name, r.last_name FROM research_field rf 
- INNER JOIN proj_field pf
- ON rf.field_id = pf.field_id
- INNER JOIN project p
- ON pf.proj_id = p.proj_id  
- INNER JOIN researcher r 
- ON p.ass_res_id = r.res_id 
- WHERE rf.field_name = 'Engineering' AND (p.start <= current_date() AND p.end >= DATE_SUB(current_date(), INTERVAL 1 YEAR)) )
+SELECT rf.field_name, r.res_id, r.first_name, r.last_name FROM research_field rf 
+INNER JOIN proj_field pf
+ON rf.field_id = pf.field_id
+INNER JOIN project p
+ON pf.proj_id = p.proj_id 
+INNER JOIN works w 
+ON p.proj_id = w.proj_id 
+INNER JOIN researcher r 
+ON w.res_id = r.res_id 
+WHERE rf.field_name = 'Engineering' AND (p.start <= current_date() AND p.end >= DATE_SUB(current_date(), INTERVAL 1 YEAR)) ) 
